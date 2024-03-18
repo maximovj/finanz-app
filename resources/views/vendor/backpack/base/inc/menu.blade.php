77260a2281a6693f502556649481af61898b1b0c
Victor J <maximovj@outlook.com>
2024-03-17 23:39:23 -0600

    feature/create-crud-balanzas | enable `sidebar_content`

--- resources/views/vendor/backpack/base/inc/menu.blade.php
+++ resources/views/vendor/backpack/base/inc/menu.blade.php
@@ -3,7 +3,7 @@
 <!-- =================================================== -->
 <ul class="nav navbar-nav d-md-down-none">
 
-    @if (backpack_auth()->check())
+    @if (backpack_auth()->guest())
         <!-- Topbar. Contains the left part -->
         @include(backpack_view('inc.topbar_left_content'))
     @endif
@@ -18,15 +18,8 @@
 <!-- ========================================================= -->
 <ul class="nav navbar-nav ml-auto @if(config('backpack.base.html_direction') == 'rtl') mr-0 @endif">
     @if (backpack_auth()->guest())
-        <!-- <li class="nav-item"><a class="nav-link" href=" //route('backpack.auth.login') }}">{{ trans('backpack::base.login') }}</a>
-        </li> -->
-        @if (config('backpack.base.registration_open'))
-            <!-- <li class="nav-item"><a class="nav-link" href=" //route('backpack.auth.register') }}">{{ trans('backpack::base.register') }}</a></li> -->
-        @endif
-    @else
         <!-- Topbar. Contains the right part -->
         @include(backpack_view('inc.topbar_right_content'))
-        @include(backpack_view('inc.menu_user_dropdown'))
     @endif
 </ul>
 <!-- ========== End of top menu right items ========== -->
