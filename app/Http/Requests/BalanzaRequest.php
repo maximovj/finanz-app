555d24ad2427180ff7b6069c48cd314537d1989d
Victor J <maximovj@outlook.com>
2024-03-17 23:48:53 -0600

    feature/create-crud-balanzas | enable authorize to `guest`

--- app/Http/Requests/BalanzaRequest.php
+++ app/Http/Requests/BalanzaRequest.php
@@ -14,7 +14,7 @@ class BalanzaRequest extends FormRequest
     public function authorize()
     {
         // only allow updates if the user is logged in
-        return backpack_auth()->check();
+        return backpack_auth()->guest();
     }
 
     /**
