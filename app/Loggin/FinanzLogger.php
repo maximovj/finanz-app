f460fd78b87c7ef89a577a9080d05f8dfe5d9d1b
Victor J <maximovj@outlook.com>
2024-03-23 16:25:33 -0600

    feature/create-loggin-laravel | create FinanzLogger

---
+++ app/Loggin/FinanzLogger.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Logging;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\LineFormatter;
+
+class FinanzLogger
+{
+    public function __invoke(array $config)
+    {
+        $logger = new Logger('custom');
+
+        $format = "[%datetime%] %channel%.%level_name%: %message% %context% %extra%\n";
+        $dateFormat = "Y-m-d H:i:s";
+        $formatter = new LineFormatter($format, $dateFormat);
+
+        $streamHandler = new StreamHandler(storage_path('logs/custom.log'), Logger::DEBUG);
+        $streamHandler->setFormatter($formatter);
+
+        $logger->pushHandler($streamHandler);
+
+        return $logger;
+    }
+}
