577ccd5c0e86f4d405c27bc665fce588143cb3c8
Victor J <maximovj@outlook.com>
2024-03-24 10:59:43 -0600

    feature/add-chart-monto-from-muestreo | modify chart effect
    modify chart effect for `muestreo` group by `monto`

--- public/js/custom/chart-monto.js
+++ public/js/custom/chart-monto.js
@@ -1,41 +1,44 @@
 
 function initChartMonto(ctx, montos_iniciales, montos_finales)
 {
-
     const data = montos_iniciales;
     const data2 = montos_finales;
 
-    const totalDuration = 10000;
-    const delayBetweenPoints = totalDuration / data.length;
+    const helpers = Chart.helpers;
+    let easing = helpers.easingEffects.easeInQuart;
+    let restart = false;
+    const totalDuration = 5000;
+    const duration = (ctx) => easing(ctx.index / data.length) * totalDuration / data.length;
+    const delay = (ctx) => easing(ctx.index / data.length) * totalDuration;
     const previousY = (ctx) => ctx.index === 0 ? ctx.chart.scales.y.getPixelForValue(100) : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y;
     const animation = {
         x: {
-          type: 'number',
-          easing: 'linear',
-          duration: delayBetweenPoints,
-          from: NaN, // the point is initially skipped
-          delay(ctx) {
+            type: 'number',
+            easing: 'linear',
+            duration: duration,
+            from: NaN, // the point is initially skipped
+            delay(ctx) {
             if (ctx.type !== 'data' || ctx.xStarted) {
-              return 0;
+                return 0;
             }
             ctx.xStarted = true;
-            return ctx.index * delayBetweenPoints;
-          }
+            return delay(ctx);
+            }
         },
         y: {
-          type: 'number',
-          easing: 'linear',
-          duration: delayBetweenPoints,
-          from: previousY,
-          delay(ctx) {
+            type: 'number',
+            easing: 'linear',
+            duration: duration,
+            from: previousY,
+            delay(ctx) {
             if (ctx.type !== 'data' || ctx.yStarted) {
-              return 0;
+                return 0;
             }
             ctx.yStarted = true;
-            return ctx.index * delayBetweenPoints;
-          }
+            return delay(ctx);
+            }
         }
-      };
+    };
 
     new Chart(ctx, {
         type: 'line',
@@ -59,7 +62,11 @@ function initChartMonto(ctx, montos_iniciales, montos_finales)
             intersect: false
           },
           plugins: {
-            legend: false
+            legend: false,
+            title: {
+              display: true,
+              text: () => easing.name
+            }
           },
           scales: {
             x: {
@@ -67,5 +74,5 @@ function initChartMonto(ctx, montos_iniciales, montos_finales)
             }
           }
         }
-    });
+      });
 }
